f8eee5fbd2d5001717cd819fd531f48617a28a51
emanueloliveiraandrade <emanueloliveiraandrade@alunos.utfpr.edu.br>
2024-11-01 16:51:15 -0300

    chore: add bootstrap icons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NgIconComponent, provideIcons } from '@ng-icons/core';
+import { bootstrapAirplane } from '@ng-icons/bootstrap-icons';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgIconComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  providers: [provideIcons({ bootstrapAirplane })],
 })
 export class AppComponent {
+  icon = bootstrapAirplane;
   title = 'lego-tech';
 }
