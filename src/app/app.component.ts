f0a182c2a7d9904b1d08e771a5e8bf415e94e307
Laira Zaine <lairazaine@outlook.com>
2024-11-01 17:55:13 -0300

    feature: add header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NgIconComponent, provideIcons } from '@ng-icons/core';
 import { bootstrapAirplane } from '@ng-icons/bootstrap-icons';
+import { HeaderComponent } from './header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NgIconComponent],
+  imports: [RouterOutlet, NgIconComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
   providers: [provideIcons({ bootstrapAirplane })],
