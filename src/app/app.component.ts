9cd46f7d2748ced245a06a9fa12bf4edf5d564bd
emanueloliveiraandradeutfpr <emanueloliveiraandrade@alunos.utfpr.edu.br>
2024-11-11 20:12:32 -0300

    fix: router-outlet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,19 @@ import { RouterOutlet } from '@angular/router';
 import { NgIconComponent, provideIcons } from '@ng-icons/core';
 import { bootstrapAirplane } from '@ng-icons/bootstrap-icons';
 import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NgIconComponent, HomeComponent],
+  imports: [
+    RouterOutlet,
+    NgIconComponent,
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
   providers: [provideIcons({ bootstrapAirplane })],
