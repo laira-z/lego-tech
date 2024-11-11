0631c93ee58756eea68511dc2e3227e1f5c74ced
emanueloliveiraandradeutfpr <emanueloliveiraandrade@alunos.utfpr.edu.br>
2024-11-11 19:55:12 -0300

    feat: add card-item

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
 import { FooterComponent } from '../footer/footer.component';
+import { CardItemComponent } from '../card-item/card-item.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent, FooterComponent],
+  imports: [HeaderComponent, FooterComponent, CardItemComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
