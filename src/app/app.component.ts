505ccc257952ca40bd596606d1cef7dbd1638871
emanueloliveiraandradeutfpr <emanueloliveiraandrade@alunos.utfpr.edu.br>
2024-11-11 19:41:50 -0300

    feat: add home

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NgIconComponent, provideIcons } from '@ng-icons/core';
 import { bootstrapAirplane } from '@ng-icons/bootstrap-icons';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NgIconComponent, HeaderComponent, FooterComponent],
+  imports: [RouterOutlet, NgIconComponent, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
   providers: [provideIcons({ bootstrapAirplane })],
