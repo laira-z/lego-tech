251a94b04549310ac9e7396655af938b0949a2f0
emanueloliveiraandrade <emanueloliveiraandrade@alunos.utfpr.edu.br>
2024-11-15 13:43:18 -0300

    headerr

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { NgIconComponent, provideIcons } from '@ng-icons/core';
+import { provideIcons } from '@ng-icons/core';
 import { bootstrapAirplane } from '@ng-icons/bootstrap-icons';
 import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './header/header.component';
@@ -9,13 +9,7 @@ import { FooterComponent } from './footer/footer.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [
-    RouterOutlet,
-    NgIconComponent,
-    HomeComponent,
-    HeaderComponent,
-    FooterComponent,
-  ],
+  imports: [RouterOutlet, HomeComponent, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
   providers: [provideIcons({ bootstrapAirplane })],
