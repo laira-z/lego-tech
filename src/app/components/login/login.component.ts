dc5f7ce9a7aeebbf432e6c43488d830a9b205a75
daniel0052 <daniel.farias.lima@gmail.com>
2025-02-03 19:57:09 -0300

    fix: login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,26 +1,43 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import {
+  FormControl,
+  FormGroup,
+  Validators,
+  ReactiveFormsModule,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
 export class LoginComponent {
   loginForm = new FormGroup({
-    name: new FormControl('', Validators.minLength(5)),
-    email: new FormControl('', Validators.email),
-    password: new FormControl('', Validators.minLength(4)),
+    name: new FormControl('', [Validators.minLength(5), Validators.required]),
+    email: new FormControl('', [Validators.email, Validators.required]),
+    password: new FormControl('', [
+      Validators.minLength(4),
+      Validators.required,
+    ]),
   });
 
   logar() {
     console.log('Entrou!!');
     console.log(this.loginForm);
   }
-  onSubmit() {
+  Submit() {
     // TODO: Use EventEmitter with form value
     console.warn(this.loginForm.value);
+  }
+
+  userForm = new FormGroup({
+    username: new FormControl('', Validators.required),
+    email: new FormControl('', [Validators.required, Validators.email]),
+  });
+
+  onSubmit() {
+    console.log(this.userForm.value);
   }
 }
