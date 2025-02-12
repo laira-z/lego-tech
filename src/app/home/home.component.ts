b9275b5ff7f1aac6a26017adb2fcdd29148d442a
emanueloliveiraandrade <emanueloliveiraandrade@alunos.utfpr.edu.br>
2025-02-11 22:17:16 -0300

    feat: create carrousel component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CardListComponent } from '../components/card-list/card-list.component';
+import { CarouselComponent } from '../components/carousel/carousel.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CardListComponent],
+  imports: [CardListComponent, CarouselComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
